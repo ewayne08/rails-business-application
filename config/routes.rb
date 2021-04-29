bd6f444d20a17e3cf2ff2bdf1108ac2046ca7bd2
Ethan <ethanchristianwayne@gmail.com>
2021-04-29 16:20:35 -0400

    message

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+  root to: 'sessions#welcome'
+
+  get 'auth/google_oauth2/callback', to: 'sessions#omniauth'
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  get '/signup', to: 'users#new' # controller and action
+  post '/signup', to: 'users#create'
+  resources :users, except: [:destroy]
+  
+  get '/companies/recent_companies' => 'companies#recent_companies' , as: 'recent_companies'
+
+  
+  resources :jobs
+  resources :companies do
+    resources :jobs
+  end
+
+  match '*path' => 'application_controller#fallback', via: [:all] 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
